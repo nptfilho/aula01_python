4e3f5a24fd552b2e2d94216f85f44ecb625f309a
nptfilho <nelsonpeixoto@hotmail.com.br>
2023-05-10 20:47:21 -0300

    Adicionando o arquivo inicio.py

---
+++ inicio.py
@@ -0,0 +1,10 @@
+"""""
+print("Olá Mundo")
+nome = input("Digite o seu nome ")
+print("O nome digitado foi: " + nome)
+"""""
+
+numero = int(input("Digite um numero: "))
+soma = numero + 10
+#print(type(numero))
+print(f"O resultado da soma é: {soma}")
