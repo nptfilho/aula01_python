ff88429993b966e3948de95919825d99fc8acfae
nptfilho <nelsonpeixoto@hotmail.com.br>
2023-05-10 20:54:00 -0300

    Modificando o arquivo Exercicio.py

--- exercico1.py
+++ exercico1.py
@@ -1,5 +1,6 @@
 nome = input(print("Qual é o seu nome?"))
 idade = int(input("Agora digite sua idade?"))
+print("Eu sou o Nelson")
 print(f"Olá: {nome}, sua idade atualmente é {idade} \n")
 
 print("Fim do exercicio 1")
