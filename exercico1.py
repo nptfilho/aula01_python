1cbefb9f0e717c9c2bdcfdf7a86d8c2e0156fda0
nptfilho <nelsonpeixoto@hotmail.com.br>
2023-05-10 20:37:55 -0300

    Incluindo o arquivo exercicio1.py

---
+++ exercico1.py
@@ -0,0 +1,23 @@
+nome = input(print("Qual é o seu nome?"))
+idade = int(input("Agora digite sua idade?"))
+print(f"Olá: {nome}, sua idade atualmente é {idade} \n")
+
+print("Fim do exercicio 1")
+print("------------------")
+
+num_a = int(input("Digite o primeiro numero "))
+num_b = int(input("Digite o segundo numero "))
+soma = num_a + num_b
+print(f"A soma dos dois números é: {soma}")
+
+print("Fim do exercicio 2")
+print("------------------")
+
+#frase = frase.upper(input(print("Digite uma frase ")))
+#print(frase)
+
+texto = "Curso Python Progressivo"
+print(texto)
+
+texto = texto.lower()
+print(texto)
